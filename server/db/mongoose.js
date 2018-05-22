c73ee96fc92999f26ebb6433ed75c13f54ae3617
iamsss <saurav9760@gmail.com>
2018-05-22 18:09:50 +0530

    little change

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,15 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://saurav9760:sfb604#Saurav@ds231460.mlab.com:31460/todoapi');
+var path = 'mongodb://saurav9760:sfb604#Saurav@ds231460.mlab.com:31460/todoapi';
+try
+{
+    mongoose.connect(path);
+}
+catch(e)
+{
+    console.log("Failed to connect to database",e);
+}
 
 module.exports = {
     mongoose
