364dc7629f24d4422c4b41c862a4b6b4cafdfc0e
iamsss <saurav9760@gmail.com>
2018-05-22 18:53:50 +0530

    editing moongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,15 @@ mongoose.Promise = global.Promise;
 process.env.MONGODB_URI = 'mongodb://saurav9760:sfb604#Saurav@ds231460.mlab.com:31460/todoapi';
 try
 {
-    mongoose.connect(process.env.MONGODB_URI || 3000);
+    mongoose.connect(process.env.MONGODB_URI || 3000, function (err) {
+        // Log Error
+        if (err) {
+        console.error(chalk.red('Could not connect to MongoDB!'));
+        console.log(err);
+        } else {
+        
+        }
+        });
 }
 catch(e)
 {
