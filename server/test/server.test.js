5c4e41acf680af29a12deb9fd921b776fafd9d1e
iamsss <saurav9760@gmail.com>
2018-05-22 23:42:07 +0530

    Add Patch to Api

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -92,8 +92,66 @@ describe('POST /todos', () => {
     //     })
     // })
 
-    it('Should Found The Result', (done)=> {
-        var Id ='5b03c5e51215f7641c51386b';
+    // it('Should Found The Result', (done)=> {
+    //     var Id ='5b03c5e51215f7641c51386b';
+    //     request(app)
+    //     .get('/todos/'+ Id)
+    //     .expect(200)
+    //     .end((err,res) => {
+    //         if(err){
+    //             return done(err);
+    //         }
+    //         Todo.find().then((todos) => {
+    //             expect(todos.length).toBe(1);
+    //             expect(todos[0].text).toBe('this is from PostMAn 1');
+    //             done();
+    //         }).catch((e) => done(e))
+            
+           
+    //     })
+    // })
+
+    // it('should return 404 Not Found When Invalid Id Send', (done)=> {
+    //     var InvalidId ='6b03bed9b5d0ef381164123f';
+    //     request(app)
+    //     .get('/todos/'+ InvalidId)
+    //     .expect(404)
+    //     .end((err,res) => {
+    //         if(err){
+    //             return done(err);
+    //         }
+    //         Todo.find().then((todos) => {
+                
+    //             done();
+    //         }).catch((e) => done(e))
+            
+           
+    //     })
+    // })
+
+
+    // it('should return 400 When Invalid ID send', (done)=> {
+    //     var id ='qwer';
+    //     request(app)
+    //     .get('/todos/' + id)
+    //     .expect(400)
+    //     .end((err,res) => {
+    //         if(err){
+    //             return done(err);
+    //         }
+    //         Todo.find().then((todos) => {
+    //             done();
+    //         }).catch((e) => done(e))
+            
+           
+    //     })
+    // })
+
+
+
+
+    it('Should Delete The Result', (done)=> {
+        var Id ='5b0441e3d94b38181f0e3fb0';
         request(app)
         .get('/todos/'+ Id)
         .expect(200)
@@ -102,8 +160,7 @@ describe('POST /todos', () => {
                 return done(err);
             }
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(1);
-                expect(todos[0].text).toBe('this is from PostMAn 1');
+                expect(todos[0].text).toBe('this is from PostMAn 2');
                 done();
             }).catch((e) => done(e))
             
