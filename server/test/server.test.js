6a08079f6ee09f39d8c22137553865f3d7f2f327
iamsss <saurav9760@gmail.com>
2018-05-22 13:02:29 +0530

    testing todo Get By Id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+
 // Dummy Data
 const todos = [{
     text: 'First test Todo'
@@ -11,11 +12,9 @@ const todos = [{
     text: 'Second test todos'
 }]
 
-beforeEach((done) => {
-    Todo.remove({}).then(()=> {
-        return Todo.insertMany(todos);
-    }).then(() => done());
-}); // Just to clear data
+// beforeEach((done) => {
+//     Todo.remove({}).then(() => done());
+// }); // Just to clear data
 describe('POST /todos', () => {
     // it('should create a new todo', (done) => {
     //     var text ='Test';
@@ -56,17 +55,55 @@ describe('POST /todos', () => {
     // })
 
 
-    it('should have two items in todos', (done)=> {
-        var text ='';
+    // it('should have two items in todos', (done)=> {
+    //     var text ='';
+    //     request(app)
+    //     .get('/todos')
+    //     .expect(200)
+    //     .end((err,res) => {
+    //         if(err){
+    //             return done(err);
+    //         }
+    //         Todo.find().then((todos) => {
+    //             expect(todos.length).toBe(2);
+    //             done();
+    //         }).catch((e) => done(e))
+            
+           
+    //     })
+    // })
+
+    // it('should have these todos', (done)=> {
+    //     var text1 ='First test Todo';
+    //     request(app)
+    //     .get('/todos')
+    //     .expect(200)
+    //     .end((err,res) => {
+    //         if(err){
+    //             return done(err);
+    //         }
+    //         Todo.find().then((todos) => {
+    //             expect(todos.length).toBe(2);
+    //             expect(todos[0].text).toBe(text1);
+    //             done();
+    //         }).catch((e) => done(e))
+            
+           
+    //     })
+    // })
+
+    it('Should Found The Result', (done)=> {
+        var Id ='5b03c5e51215f7641c51386b';
         request(app)
-        .get('/todos')
+        .get('/todos/'+ Id)
         .expect(200)
         .end((err,res) => {
             if(err){
                 return done(err);
             }
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(2);
+                expect(todos.length).toBe(1);
+                expect(todos[0].text).toBe('this is from PostMAn 1');
                 done();
             }).catch((e) => done(e))
             
@@ -74,18 +111,35 @@ describe('POST /todos', () => {
         })
     })
 
-    it('should have these todos', (done)=> {
-        var text1 ='First test Todo';
+    it('should return 404 Not Found When Invalid Id Send', (done)=> {
+        var InvalidId ='6b03bed9b5d0ef381164123f';
         request(app)
-        .get('/todos')
-        .expect(200)
+        .get('/todos/'+ InvalidId)
+        .expect(404)
         .end((err,res) => {
             if(err){
                 return done(err);
             }
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(2);
-                expect(todos[0].text).toBe(text1);
+                
+                done();
+            }).catch((e) => done(e))
+            
+           
+        })
+    })
+
+
+    it('should return 400 When Invalid ID send', (done)=> {
+        var id ='qwer';
+        request(app)
+        .get('/todos/' + id)
+        .expect(400)
+        .end((err,res) => {
+            if(err){
+                return done(err);
+            }
+            Todo.find().then((todos) => {
                 done();
             }).catch((e) => done(e))
             
