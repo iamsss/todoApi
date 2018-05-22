d567716017fc3f87bf12bef36f60c7a68a5b1d36
iamsss <saurav9760@gmail.com>
2018-05-23 01:16:24 +0530

    Creating User Model

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,33 @@
-var mongoose = require('mongoose');
+const  mongoose = require('mongoose');
+const validator = require('validator');
+
+
 var User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true // for triming starting and ending white space
-    }
+        trim: true, // for triming starting and ending white space
+        unique: true,
+        validator: validator.isEmail,
+        message: '{VALUE} is not a valid email'
+
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+                },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
  
 module.exports = {
