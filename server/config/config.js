829242ff48fb02025b9233f843f43de8c17aacf4
iamsss <saurav9760@gmail.com>
2018-05-23 19:04:06 +0530

    updating online Database for production

--- server/config/config.js
+++ server/config/config.js
@@ -9,4 +9,7 @@ if(env === 'development'){
     process.env.PORT = '3000';
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodosTest';
     
+}else{
+    process.env.MONGODB_URI = 'mongodb://saurav9760:sfb604#Saurav@ds231460.mlab.com:31460/todoapi';
+    
 }
